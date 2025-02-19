231f33a6cf917a6d3c569fda98344ffedfaac8fb
sayoga <yogasuryaadi@gmail.com>
2025-02-19 16:42:42 +0700

    membuat filter data produk

--- index.js
+++ index.js
@@ -20,46 +20,52 @@ app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.send('index');
-    });
+});
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
-    });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+});
     
 app.get('/products/create', (req, res) => {
     res.render('products/create');
-    });
+});
 
 app.post('/products', async (req, res) => {
     const product = new Product(req.body);
     await product.save();
     res.redirect(`/products/${product._id}`);
-    });
+});
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product });
-    });
+});
 
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product });
-    });
+});
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true});
     res.redirect(`/products/${product._id}`);
-    });
+});
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
     res.redirect('/products');
-    });
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
